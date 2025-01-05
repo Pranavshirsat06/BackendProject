3a0ff5750d781efa00d0474f82ad946e54aea4d4
Pranav <pranavshirsat06@gmail.com>
2025-01-05 20:55:38 +0530

    all setup done

--- src/app.js
+++ src/app.js
@@ -18,4 +18,10 @@ app.use(express.static("public")); //to access the public folder
 
 app.use(cookieParser()); //to parse the cookie
 
+//routes
+import userRoutes from "./routes/user.routes.js";
+
+//routes decclaration
+app.use("/api/v1/users", userRoutes);
+
 export default app;
